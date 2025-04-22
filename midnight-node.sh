45e5d3032e20bcaee21e471f3a8d213796ad7720
Giles Cope <gilescope@gmail.com>
2025-04-22 19:01:42 +0100

    feat: if direnv isn't working give an appropriate error message.

--- midnight-node.sh
+++ midnight-node.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ -z "$MIDNIGHT_NODE_IMAGE" ]; then
+  echo "Error: Env var MIDNIGHT_NODE_IMAGE is not set or is empty"
+  echo "Please install direnv and run `direnv allow` to activate it."
+  exit 1
+fi
+
 docker run -it \
   -e CFG_PRESET=${CFG_PRESET} \
   -e DB_SYNC_POSTGRES_CONNECTION_STRING=${DB_SYNC_POSTGRES_CONNECTION_STRING} \
