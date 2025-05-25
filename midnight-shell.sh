8f2e24afc75c7e2ab936336841a17a89d679baf3
Giles Cope <gilescope@gmail.com>
2025-05-25 09:47:00 +0100

    fix: more sensible to shell into existing container.

--- midnight-shell.sh
+++ midnight-shell.sh
@@ -8,21 +8,4 @@
 # ./midnight-node wizards register1  USE Ogmios hostname: host.docker.internal
 #
 
-if [ -z "$MIDNIGHT_NODE_IMAGE" ]; then
-  echo "Error: Env var MIDNIGHT_NODE_IMAGE is not set or is empty"
-  echo "Please install direnv and run 'direnv allow' to activate it."
-  exit 1
-fi
-
-if [[ "$(uname)" == "Darwin" ]]; then
-  # For arm mac persuade docker to be more liberal:
-  export DOCKER_DEFAULT_PLATFORM=linux/arm64
-fi
-
-docker run -it \
-  -e CFG_PRESET="${CFG_PRESET}" \
-  -e DB_SYNC_POSTGRES_CONNECTION_STRING="${DB_SYNC_POSTGRES_CONNECTION_STRING}" \
-  -v ./data:/data \
-  -v "./envs/${CFG_PRESET}/pc-chain-config.json:/pc-chain-config.json" \
-  --entrypoint bash \
-  "${MIDNIGHT_NODE_IMAGE}"
+docker exec -it midnight bash
