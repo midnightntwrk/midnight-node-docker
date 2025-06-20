fb6fc1dcdded6ecd5aae806bbd99fa059ad5e04d
Stevan Lohja <stevan.lohja@iohk.io>
2025-06-20 15:43:39 -0400

    fix: fix mnode scripts

--- midnight-node.sh
+++ midnight-node.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env bash
-
-if [ -z "$MIDNIGHT_NODE_IMAGE" ]; then
-  echo "Error: Env var MIDNIGHT_NODE_IMAGE is not set or is empty"
-  echo "Please install direnv and run 'direnv allow' to activate it."
-  exit 1
-fi
-
-docker exec -t midnight /midnight-node "$@"
+docker run -it \
+  --name midnight \
+  -e CFG_PRESET="${CFG_PRESET}" \
+  -e DB_SYNC_POSTGRES_CONNECTION_STRING="${DB_SYNC_POSTGRES_CONNECTION_STRING}" \
+  -v ./data:/data \
+  -v "./envs/${CFG_PRESET}/pc-chain-config.json:/pc-chain-config.json" \
+  --entrypoint bash \
+  "${MIDNIGHT_NODE_IMAGE}"
