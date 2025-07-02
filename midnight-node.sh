e6954345948f4564f050c8962cde7088a334535c
Stevan Lohja <stevan.lohja@iohk.io>
2025-07-02 09:07:38 -0400

    fix: update midnight-node script

--- midnight-node.sh
+++ midnight-node.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-# Docker compose up midnight-node interactively in shell.
-docker compose -f compose.yml up
+if [ -z "$MIDNIGHT_NODE_IMAGE" ]; then
+  echo "Error: Env var MIDNIGHT_NODE_IMAGE is not set or is empty"
+  echo "Please install direnv and run 'direnv allow' to activate it."
+  exit 1
+fi
+
+docker exec -t midnight /midnight-node "$@"
