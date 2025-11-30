8375a99143fa0b40a281c817fc7ab554e32c5d0c
Giles Cope <gilescope@gmail.com>
2025-11-30 09:12:38 +0000

    feat: scan images in one job so can matrix over environments

---
+++ .github/scan.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+source "./.envrc"
+
+docker compose -f ./compose.yml -f ./compose-partner-chains.yml build
+
+scan_image() {
+  local image="$1"
+  echo "Scanning $image..."
+  local SAFE_NAME=$(echo "$image" | sed 's/[\/:]/-/g')
+  local SARIF_FILE="${SAFE_NAME}.sarif"
+
+  time docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v trivy-cache:/root/.cache \
+    -v "$(pwd):/output" \
+    aquasec/trivy:0.67.2 image \
+    --format sarif \
+    --output "/output/$SARIF_FILE" \
+    "$image"
+
+  echo "Completed $SARIF_FILE"
+}
+export -f scan_image
+
+docker compose -f ./compose.yml -f ./compose-partner-chains.yml config --images | \
+  xargs -I {} bash -c 'scan_image "$@"' _ {}
