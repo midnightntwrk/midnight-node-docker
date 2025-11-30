19453692f674c76c896045dd307274639034ac07
Giles Cope <gilescope@gmail.com>
2025-11-30 10:04:00 +0000

    feat: skip unfixed

--- .github/scan.sh
+++ .github/scan.sh
@@ -16,12 +16,20 @@ scan_image() {
     -v "$(pwd):/output" \
     aquasec/trivy:0.67.2 image \
     --format sarif \
+    --ignore-unfixed \
+    --no-progress \
     --output "/output/$SARIF_FILE" \
     "$image"
-
+  jq --arg image "$image" \
+   '.runs[0].automationDetails = {
+     id: "trivy/\($image)",
+     description: {text: "Trivy scan for \($image)"}
+   }' $SARIF_FILE > ./scan_reports/${SARIF_FILE}
   echo "Completed $SARIF_FILE"
 }
 export -f scan_image
+
+mkdir scan_reports
 
 docker compose -f ./compose.yml -f ./compose-partner-chains.yml config --images | \
   xargs -I {} bash -c 'scan_image "$@"' _ {}
